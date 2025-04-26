5b8888fc41cdc76ac2bd693f67e52864517956ee
Marion Saru <maghangasaru@gmail.com>
2025-04-26 19:29:55 +0300

    Completed optional lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "Lohola";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  // Overwrite the existing global bestCustomer
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  // Attempting to reassign a const will throw an error (expected behavior)
+  leastFavoriteCustomer = "rohoni";
+}
+
+
